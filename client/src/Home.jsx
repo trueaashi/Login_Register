fe9a7b54cbc553cee6d726f133b3ad9f384dc87b
trueaashi <trueaashi@gmail.com>
2024-05-28 22:31:59 +0530

    Resolved merge conflict

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,22 +1,3 @@
-// import React from 'react'
-
-// function Home () {
-//   return (
-//     <div className='color-red'>
-// <h2>
-//         HOME
-//     </h2>
-//     <p>
-//         Welcome to our simple and secure registration and login website built with the powerful MERN stack: MongoDB, Express, React, and Node.js.
-//         This application is designed to provide a seamless user experience with robust authentication features. MongoDB serves as our database, ensuring efficient storage and retrieval of user information. Express and Node.js power our backend, managing user requests and handling authentication processes. On the frontend, React offers a dynamic and responsive interface, making it easy for users to register, log in, and navigate through the site. Whether you're creating a new account or accessing your existing profile, our system is designed to keep your data safe and provide a smooth, intuitive experience. Join us and explore the simplicity and efficiency of modern web development with the MERN stack
-//     </p>
-//     </div>
-    
-//   )
-// }
-
-// export default Home;
-
 import React from 'react';
 
 function Home() {
